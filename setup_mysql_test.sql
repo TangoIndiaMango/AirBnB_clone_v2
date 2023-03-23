b2cc4e106a1c2306827f742c5194e7854cfc7403
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-03-22 22:26:19 -0500

    testdb

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- create database hbnb_test_deb
+CREATE DATABASE IF NOT EXISTS hbnh_test_db;
+
+-- create user hbnb_test with password hbnb_test_pwd
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- grant all previledges to hbnb_test on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- grant select previlege to hbnb_test on performance_schema
+GRANT SELECT ON preformance_schema.* TO 'hbnb_test'@'localhost';
