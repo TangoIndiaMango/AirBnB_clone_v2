c1dff50914cf7448b9a194bd8bd6b8c2ad889b16
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-03-23 18:46:45 -0500

    fixed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -8,5 +8,5 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- grant select previlege to hbnb_dev on performance_schema
-GRANT SELECT ON preformance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
